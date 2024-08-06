74e3eb3b832c2da107143701e3268f3f0ddbeee2
ybronny <ronnymwangi7@gmail.com>
2024-08-06 14:24:35 +0300

    removing merge and fixing placements of codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import HomePage from './HomePage';
+import RentPage from './RentPage';
+import BuyPage from './BuyPage';
+import Login from './Login';
+import SignUp from './SignUp';
+
 function App() {
   return (
     <>
-
+   <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/rent" element={<RentPage />} />
+        <Route path="/buy" element={<BuyPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
     </>
   )
 }
